df8c0899ac0f5e9130c520f63df87b9fb4430458
cdynjm <jemuelcadayona3@gmail.com>
2025-05-28 15:34:41 +0800

    update

--- resources/js/components/SlowLink.vue
+++ resources/js/components/SlowLink.vue
@@ -1,42 +1,40 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import { router, Link } from '@inertiajs/vue3';
 
 const props = defineProps({
   href: { type: String, required: true },
-  as: { type: String, default: 'a' },
-  method: String,
-  data: Object,
-  headers: Object,
-  preserveScroll: Boolean,
-  preserveState: [Boolean, String],
-  replace: Boolean,
-  only: Array,
-  prefetch: Boolean,
-  active: Boolean,
   class: {
     type: [String, Object, Array],
     default: '',
   },
 });
 
-const linksEnabled = ref(false);
+const loading = ref(false);
 
-onMounted(() => {
+function handleClick(event: Event) {
+  event.preventDefault();
+
+  if (loading.value) return;
+
+  loading.value = true;
+
   setTimeout(() => {
-    linksEnabled.value = true;
-  }, 0); 
-});
+    router.visit(props.href);
+    loading.value = false;
+  }, 1500);
+}
 </script>
 
 <template>
-  <Link
+  <a
     :href="props.href"
     :class="[
       props.class,
-      { 'pointer-events-none': !linksEnabled }
+      { 'pointer-events-none opacity-50': loading }
     ]"
+    @click="handleClick"
   >
     <slot />
-  </Link>
+  </a>
 </template>
